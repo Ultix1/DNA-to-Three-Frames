7578c0fa96d9a6d22bf3ae866bc74d76ae2dbba0
Justin-Aye <balcera85@gmail.com>
2024-03-10 19:16:40 +0800

    Refactored params variable

--- learning/utils/constants.py
+++ learning/utils/constants.py
@@ -28,6 +28,18 @@ CODON_TABLE = {
     "GGT" : "G", "GGC" : "G", "GGA" : "G", "GGG" : "G",
 }
 
+PARAMS = {
+    'epsilon' : 0.99999,    # Starting Epsilon
+    'epsilon_min': 0.01,    # Minimum Epsilon
+    'decay' : 0.99,         # Epsilon Decay
+    'gamma' : 0.99,         # Discount Factor for target q vals
+    'buffer_size' : 50000,  # Size of Buffer
+    'max_ep' : 1000,        # Max training episodes
+    'batch_size' : 64,      # Batch size for training
+    'train_freq' : 100,     # How Many Steps before Updating Main Q-Network
+    'tau': 0.01             # Discount Factor for Updating Target Q-Network
+}
+
 GAP_EXTENSION_PENALTY = 2
 
 GAP_OPEN_PENALTY = 3
