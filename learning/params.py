78e10b012fe8b3562d657e8d0c68a045ed632e22
Justin-Aye <balcera85@gmail.com>
2024-07-13 19:28:08 +0800

    Adjusted Params

--- learning/params.py
+++ learning/params.py
@@ -3,14 +3,15 @@ from utils.constants import Action
 PARAMS = {
     'epsilon' : 0.99999,            # Starting Epsilon
     'epsilon_min': 0.01,            # Minimum Epsilon
-    'decay' : 0.99,                 # Epsilon Decay
+    'decay' : 0.999,                # Epsilon Decay
     'gamma' : 0.99,                 # Discount Factor for target q vals
     'buffer_size' : 50000,          # Size of Buffer
-    'max_ep' : 1000,                # Max training episodes
+    'max_ep' : 5000,                # Max training episodes
     'batch_size' : 64,              # Batch size for training
     'train_freq' : 100,             # How Many Steps before Updating Main Q-Network
     'tau': 0.01,                    # Discount Factor for Updating Target Q-Network
-    'input_shape' : (8, 22, 1),     # Input Shape
+    'window_size' : 3,              # Window Size for Input
+    'input_shape' : (16, 22, 1),    # Input Shape is (4 + (w*4), 22, 1) where w is the window size
     'lr' : 0.001,
     'actions' : [
         Action.MATCH.value, 
