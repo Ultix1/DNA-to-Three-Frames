0dca525e8f52f9c275d84f049f3b7b5276f0104f
zen-young <renzcruz50@gmail.com>
2024-07-15 00:41:18 +0800

    added sequence traceback

---
+++ learning/aligner_test.py
@@ -0,0 +1,37 @@
+from utils.aligner import ThreeFrameAligner
+from utils.sequence_gen import SeqGen
+from os import remove as remove_file
+
+if __name__ == '__main__':
+    # base_pairs = [10, 30, 60, 100, 300, 500, 800, 1000, 1500, 3000, 4500, 6000, 7500, 9000, 13500, 15000]
+    base_pairs = [30, 30, 30]
+
+    print("Three Frame Aligner Memory Test\n")
+    for base_pair_len in base_pairs:
+        retries = 10
+        while True:
+            seq_gen = SeqGen(lseqs=base_pair_len, num_sets=1)
+            try:
+                seq_gen.generate_sequences_and_proteins()
+                seq_gen.save_sequences_to_files()
+                break
+            except Exception as e:
+                if retries == 0:
+                    raise e
+                retries -= 1
+
+        with open("AA1.txt", "r") as a, open("DNA1.txt", "r") as b:
+            # dna = b.read().strip()
+            # protein = a.read().strip()
+            dna = "TTAGTTATG"
+            protein = "LVM"
+            aligner = ThreeFrameAligner()
+            _, actions, seq = aligner.align(dna, protein, debug=True)
+            print(dna)
+            print(protein)
+            print(actions)
+            print(seq)
+
+        print(f'seq_len={base_pair_len}\tave_mem_usage={float(aligner.ave_mem_usage)}')
+        remove_file("AA1.txt")
+        remove_file("DNA1.txt")
